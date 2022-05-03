de93eeced4888a38ae45a75dd93ff952f72a4803
DDD120 <dltnals9794@naver.com>
2022-05-03 20:30:04 +0900

    DESIGN Header Component CSS 수정 - position:fixed로 Header Component 상단 고정 - Head styled 요소 header로 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,14 +3,19 @@ import { FiSearch } from "react-icons/fi";
 import { BsFillPersonFill } from "react-icons/bs";
 import { Common } from "styles/common";
 
-const Head = styled.head`
-  margin: 18px 60px;
+const Head = styled.header`
+  width: 100%;
   height: 60px;
+  position: fixed;
+  top: 0;
+  background-color: ${Common.colors.beige};
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding: 20px 60px;
 `;
 
-const Logo = styled.div`
+const Logo = styled.h1`
   font-family: "Roboto", sans-serif;
   font-weight: 700;
   font-size: 1.125rem;
@@ -27,8 +32,7 @@ const NavItem = styled.div`
   width: 36px;
   height: 36px;
   color: ${Common.colors.black};
-  background-color: ${(props) =>
-    props.color ? props.color : Common.colors.cyan};
+  background-color: ${(props) => props.color};
   border-radius: 50%;
   display: flex;
   justify-content: center;
@@ -44,7 +48,7 @@ const Header = () => {
         <NavItem color={Common.colors.orange}>
           <FiSearch />
         </NavItem>
-        <NavItem>
+        <NavItem color={Common.colors.cyan}>
           <BsFillPersonFill />
         </NavItem>
       </Nav>
