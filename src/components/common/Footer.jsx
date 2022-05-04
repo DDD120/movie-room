2e94fc78cd4c9480a611bcdc4aabad78916c6e7c
DDD120 <dltnals9794@naver.com>
2022-05-04 22:50:41 +0900

    FEAT Footer Component 구현

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,42 @@
+import styled from "@emotion/styled";
+import { Common } from "styles/common";
+
+const Foot = styled.footer`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+`;
+
+const Copyright = styled.div`
+  text-align: center;
+  font-size: 0.75rem;
+`;
+
+const Title = styled.span`
+  font-weight: 700;
+  margin-right: 4px;
+`;
+
+const Box = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: ${(props) => props.color};
+`;
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const getYear = new Date().getFullYear();
+
+  return (
+    <Foot>
+      <Copyright>
+        <Title>MOVIE ROOM</Title>
+        <span>© {getYear}. DDD120. ALL RIGHTS RESERVED</span>
+      </Copyright>
+      <Box color={Common.colors.cyan} />
+      <Box color={Common.colors.orange} />
+      <Box color={Common.colors.red} />
+    </Foot>
+  );
 };
 
 export default Footer;
--- src/styles/common.jsx
+++ src/styles/common.jsx
@@ -2,6 +2,7 @@ export const Common = {
   colors: {
     black: "#1A3642",
     beige: "#FEE0C8",
+    red: "#D0433A",
     orange: "#FD7D36",
     cyan: "#50ADB5",
   },
