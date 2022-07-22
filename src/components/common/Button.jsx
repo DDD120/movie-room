c1a9bc648a51f6144494b6e97c98b643fd7bdc46
DDD120 <dltnals9794@naver.com>
2022-07-22 14:48:56 +0900

    REFACRTOR Button Component 구성

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,18 @@
-const Button = () => {
-  return <div>Button</div>;
+import styled from "@emotion/styled";
+import { Common } from "styles/common";
+
+const Btn = styled.button`
+  font-family: "Noto Sans KR", sans-serif;
+  font-size: 1.125rem;
+  font-weight: 700;
+  cursor: pointer;
+  border-radius: 50px;
+  padding: 14px 30px;
+  background-color: ${Common.colors.cyan};
+`;
+
+const Button = ({ clickEvent, children }) => {
+  return <Btn onClick={clickEvent}>{children}</Btn>;
 };
 
 export default Button;
