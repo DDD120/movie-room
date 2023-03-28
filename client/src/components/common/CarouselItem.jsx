f562067b185d768d78f2051868022ceb46173f64
DDD120 <dltnals9794@naver.com>
2023-03-28 21:02:28 +0900

    fix props 인식 오류 해결

--- client/src/components/common/CarouselItem.jsx
+++ client/src/components/common/CarouselItem.jsx
@@ -3,24 +3,24 @@ import { breakpoint } from "styles/common";
 import { motion } from "framer-motion";
 
 const Base = styled(motion.li)`
-  width: ${({ showCount }) => showCount && `calc(100%/${showCount})`};
+  width: ${({ showcount }) => showcount && `calc(100%/${showcount})`};
   padding: 0 4px;
-  flex: 0 0 ${({ showCount }) => showCount && `calc(100%/${showCount})`};
+  flex: 0 0 ${({ showcount }) => showcount && `calc(100%/${showcount})`};
   scroll-snap-align: start;
   img {
     width: 100%;
   }
 
   @media only screen and (max-width: ${breakpoint.md}) {
-    width: ${({ showCount }) => showCount && `calc(100%/${showCount - 2})`};
-    flex: 0 0 ${({ showCount }) => showCount && `calc(100%/${showCount - 2})`};
+    width: ${({ showcount }) => showcount && `calc(100%/${showcount - 2})`};
+    flex: 0 0 ${({ showcount }) => showcount && `calc(100%/${showcount - 2})`};
   }
 `;
 
-const CarouselItem = ({ children, activeIndex, ...rest }) => {
+const CarouselItem = ({ children, activeIndex, showCount }) => {
   return (
     <Base
-      {...rest}
+      showcount={showCount}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transform: `translateX(-${activeIndex * 100}%)` }}
       transition={{
