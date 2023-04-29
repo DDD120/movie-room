f1573cbb423f9d327a64fecdbcfef8a2f83c3deb
DDD120 <dltnals9794@naver.com>
2023-04-30 00:06:18 +0900

    build 빌드 설정

--- server/server.js
+++ server/server.js
@@ -8,13 +8,13 @@ const app = express();
 const __dirname = path.resolve();
 const PORT = 8080;
 
-app.use(cookieParser());
 app.use(
   cors({
     origin: "https://ddd120.github.io",
     credentials: true,
   })
 );
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static(path.join(__dirname, "public")));
