0dce947c63cec66fb47a7717bb313206d38f9490
DDD120 <dltnals9794@naver.com>
2023-08-31 15:20:52 +0900

    fix cors 에러

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
-import cookieParser from "cookie-parser";
-import express from "express";
-import cors from "cors";
-import { userRouter, reviewRouter } from "./router/index.js";
-import path from "path";
+import cookieParser from "cookie-parser"
+import express from "express"
+import cors from "cors"
+import { userRouter, reviewRouter } from "./router/index.js"
+import path from "path"
 
-const app = express();
-const __dirname = path.resolve();
-const PORT = 8080;
+const app = express()
+const __dirname = path.resolve()
+const PORT = 8080
 
 app.use(
   cors({
-    origin: ["https://ddd120.github.io", "http://localhost:3000"],
+    origin: "https://ddd120.github.io",
     credentials: true,
   })
-);
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/static", express.static(path.join(__dirname, "public")));
-app.use("/image", express.static("./uploads"));
+)
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use("/static", express.static(path.join(__dirname, "public")))
+app.use("/image", express.static("./uploads"))
 
-app.use("/user", userRouter);
-app.use("/review", reviewRouter);
+app.use("/user", userRouter)
+app.use("/review", reviewRouter)
 
 app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
+  res.send("Server is running!")
+})
 
 app.listen(PORT, () => {
-  console.log("Server is running!");
-});
+  console.log("Server is running!")
+})
